591de597e15d3693faf307f90d7968d84f479b2a
fai-sas <craftedbyfaisal@gmail.com>
2024-08-24 20:29:26 +0600

    react router dom and redux added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import { store, persistor } from './redux/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
+import router from './routes/routes.tsx'
+import { Provider } from 'react-redux'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+      <Toaster />
+    </Provider>
+  </StrictMode>
 )
